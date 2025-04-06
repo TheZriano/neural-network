a38801db0fb873f8b6dad417eab5bdeef5f858bf
Giacomo Montenegro <giacomo.montenegro09@gmail.com>
2025-04-06 16:56:43 +0200

    training 26k

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import json
 import math
 import random
+import tensorflow as tf
+from tqdm import tqdm
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Carica il dataset MNIST
+(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
+
+print(f"Numero di immagini di training: {train_images.shape[0]}")
+print(f"Dimensione di una singola immagine: {train_images.shape[1:]}")
+
 
 class Neuron:
-    def __init__(self, column, weights=0, value=0):
+    def __init__(self, column, weights=0, value=0, bias=0):
         self.column=column
         self.weights=weights
-        self.bias=1
+        self.bias=bias
         self.value=value
+        self.delta=0
 
     def calc(self):
         if self.column==0:
@@ -17,7 +29,23 @@ class Neuron:
             weighted_sum = sum(neuron.calc() * weight for neuron, weight in zip(previous_neurons, self.weights))
             return function(weighted_sum+self.bias)
         
+    def get_data(self):
+        return {
+            'weights': self.weights.tolist() if isinstance(self.weights, np.ndarray) else self.weights,
+            'bias': self.bias,
+            'value': self.value.tolist() if isinstance(self.value, np.ndarray) else self.value
+        }
 
+def saveNetworkToJson(path):
+    network_data = []
+    for column in neuralNetwork:
+        column_data = []
+        for neuron in column:
+            column_data.append(neuron.get_data())
+        network_data.append(column_data)
+
+    with open(path, "w", encoding="utf-8") as file:
+        json.dump(network_data, file, indent=4)
 
 def createNetworkFromJson(path):
     global neuralNetwork
@@ -47,7 +75,7 @@ def softMax(values):
 
 
 def function(x):
-    return 1 / (1 + math.exp(-x))
+    return 1 / (1 + np.exp(-x))
 
 
 def derivative(x):
@@ -61,49 +89,62 @@ def train(input_values, target_values, learning_rate=0.1):
 
     outputs = [neuron.calc() for neuron in neuralNetwork[-1]]
 
-    errors = [(target - output) for target, output in zip(target_values, outputs)]
-
-    for i, neuron in enumerate(neuralNetwork[-1]):  # Per ogni neurone nel layer di output
-        output = neuron.calc()
-        delta = errors[i] * derivative(output)  # Calcola l'errore e la sua derivata
-        for j, prev_neuron in enumerate(neuralNetwork[neuron.column - 1]):
-            neuron.weights[j] += learning_rate * delta * prev_neuron.value  # Aggiorna i pesi
-
-    # 5. Aggiorna il bias
     for i, neuron in enumerate(neuralNetwork[-1]):
-        neuron.bias += learning_rate * errors[i]  # Aggiorna il bias per l'output
-    
+        output = neuron.calc()
+        error = target_values[i] - output
+        neuron.delta = error * derivative(output)
 
+    for layer_index in reversed(range(1, len(neuralNetwork) - 1)):
+        for i, neuron in enumerate(neuralNetwork[layer_index]):
+            output = neuron.calc()
+            # somma dei pesi * delta dei neuroni successivi
+            downstream = neuralNetwork[layer_index + 1]
+            error = sum(n.weights[i] * n.delta for n in downstream)
+            neuron.delta = error * derivative(output)
 
-
-
-
+    for layer_index in range(1, len(neuralNetwork)):
+        for neuron in neuralNetwork[layer_index]:
+            for j, prev_neuron in enumerate(neuralNetwork[layer_index - 1]):
+                neuron.weights[j] += learning_rate * neuron.delta * prev_neuron.calc()
+            neuron.bias += learning_rate * neuron.delta
 
 
 
 
 neuralNetwork=createNetworkFromJson("data.json")
 
+def one_hot(label):
+    arr = [0] * 10
+    arr[label] = 1
+    return arr
 
-for epoch in range(1000):
-    # Addestra la rete con vari esempi
-    train([1, 0], [1])  # Eseguiamo un esempio di training con input [1, 0] e target 1
-    train([0, 1], [0])  # Eseguiamo un altro esempio di training
-    train([1, 1], [0])  # Eseguiamo un altro esempio
-    train([0, 0], [0])  # Eseguiamo un altro esempio
+for i in tqdm(range(26000,36000)):  # esempio: primi 1000
+    input_data = [x / 255 for x in train_images[i].flatten()]      # normalizza
+    target = one_hot(train_labels[i])                    # etichetta
+    train(input_data, target)
+    if i%10==0:
+        saveNetworkToJson("data.json")
 
 
 
 
-test_input = [1, 0]  # Un input per il test
-train(test_input, [1])  # Allena la rete con questo input e il valore atteso
+test_input = [x / 255 for x in test_images[1].flatten()]
 
-# Ottieni e stampa i risultati
+
+for i, v in enumerate(test_input):
+    neuralNetwork[0][i].value = v
+
 output = [neuron.calc() for neuron in neuralNetwork[-1]]
-output = softMax(output)  # Applica softmax per ottenere probabilità
+output = softMax(output)
+
 for i, result in enumerate(output):
-    print(f"Classe {i}: {result*100:.2f}%")
+    print(f"Cifra {i}: {result*100:.2f}%")
 
 
+# Converte la lista 1D in una matrice 2D (28x28 per MNIST)
+input_data_2d = np.array(test_input).reshape(28, 28)
 
-#da aggiungere piu livelli
+# Visualizza l'immagine
+plt.imshow(input_data_2d, cmap='gray')  # 'gray' per immagini in bianco e nero
+plt.axis('off')  # Disattiva gli assi
+plt.show()
